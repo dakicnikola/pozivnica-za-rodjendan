31070e09d3ecdc072556e3fe9034728edce83682
Nikola Dakic <nikola.dakic.94@gmail.com>
2025-08-23 23:47:54 +0200

    Reverted changes and adjustments

--- pozivnica/src/app/magdin-prvi-rodjendan/GroundBg.tsx
+++ pozivnica/src/app/magdin-prvi-rodjendan/GroundBg.tsx
@@ -1,16 +1,16 @@
+import Image from "next/image";
+
 const Balloon = () => {
   return (
-    <div className="h-screen overflow-hidden absolute w-full aspect-[3/3] z-1 bg-[#568dbd]">
-      <video
-        className="object-cover w-full h-full [mask-image:linear-gradient(to_bottom,transparent_0,black_100px,black_100%)] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:linear-gradient(to_bottom,transparent_0,black_100px,black_100%)]"
-        autoPlay
-        loop
-        muted
-        playsInline
-      >
-        <source src="/cake-video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <div className="absolute w-full aspect-[3/3] z-1 bg-[#eaf0f3]">
+      <Image
+        src="/ground-bg.png"
+        alt=""
+        fill
+        priority
+        sizes="(max-width: 640px) 100vw, (max-width: 1024px) 90vw, 1200px"
+        className="object-cover [mask-image:linear-gradient(to_bottom,transparent_0,black_15px,black_100%)] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:linear-gradient(to_bottom,transparent_0,black_15px,black_100%)]"
+      />
     </div>
   );
 };
