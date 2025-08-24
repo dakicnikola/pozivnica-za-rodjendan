3a80a072787df9c03c2cc6d0f28bd7dc9cb187b1
Nikola Dakic <nikola.dakic.94@gmail.com>
2025-08-24 15:07:29 +0200

    Sredjen balon i magicka

--- pozivnica/src/app/magdin-prvi-rodjendan/Balloon.tsx
+++ pozivnica/src/app/magdin-prvi-rodjendan/Balloon.tsx
@@ -2,22 +2,25 @@ import Image from "next/image";
 
 const Balloon = () => {
   return (
-    <div className="relative mx-auto aspect-[2/2] w-[500px] h-[400px] scale-200">
-      {/*<div className="relative mx-auto aspect-[2/2] -left-30 top-20 scale-200 w-full max-w-sm ">*/}
+    <div className="relative mx-auto w-[500px] h-[400px] [contain:none] scale-200 -top-5">
       <Image
         src="/balloon.png"
         alt="baloon"
         fill
         sizes="(max-width: 640px) 100vw, (max-width: 1024px) 90vw, 1200px"
         className="object-contain"
+        loading="eager"
+        priority
       />
-      <div className={"relative w-15 h-10 right-0 z-50 left-[210px] top-[285px]"}>
+      <div className="relative left-[210px] top-[285px] w-[60px] h-[40px] z-50">
         <Image
           src="/magda.png"
           alt="magda"
           fill
-          sizes="(max-width: 640px) 100vw, (max-width: 102  4px) 90vw, 1200px"
-          className="object-contain z-50 [clip-path:inset(0_0_9%_0)]"
+          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 90vw, 1200px"
+          className="object-contain z-50 [clip-path:inset(0_0_9%_0)] [transform:translateZ(0)] [backface-visibility:hidden]"
+          loading="eager"
+          priority
         />
       </div>
     </div>
